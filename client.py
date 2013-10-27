77bca93e66820b6e852d273f861fc47618d78b5c
Gustomyasov Ruslan <rusik@4ege.ru>
2013-10-28 03:07:42 +0400

    md5 added to client

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
 import requests
 import sys
+import hashlib
 
 for file in sys.argv[1:-1]:
+    md5 = hashlib.md5(file).hexdigest()
+    headers = {'md5:': md5}
+    print headers
     r = requests.post(sys.argv[-1], files={file: open(file, 'rb')})
     print file
 print "done"
