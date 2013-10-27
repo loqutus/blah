f3e8012d7c2d2900c1691c24c733de9d38f64c7b
Gustomyasov Ruslan <rusik@4ege.ru>
2013-10-28 03:47:49 +0400

    test md5 headers

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             md5Local = hashlib.md5(open(item.name).read()).hexdigest()
         md5 = self.headers.get("md5")
         print md5
-        primt md5Local
+        print md5Local
         if md5 == md5Local:
             self.send_response(200)
         else:
