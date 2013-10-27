292ffd6472ef44910a9ddb337126f72eafcde501
Gustomyasov Ruslan <rusik@4ege.ru>
2013-10-27 21:03:08 +0400

    writing files

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
 import SimpleHTTPServer
 import SocketServer
+import cgi
+import os
 
-# minimal web server.  serves files relative to the
-# current directory.
 
-PORT = 8000
+class MyTCPServer(SocketServer.TCPServer):
+    allow_reuse_address = True
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST',
+                     'CONTENT_TYPE': self.headers['Content-Type'],
+            })
+        for item in form.list:
+            f = open(item.name, "w")
+            print item.name
+            f.write(item.value)
+            f.close()
+        self.send_response(200)
 
-print "serving at port", PORT
-httpd.serve_forever()
+    def do_PUT(self):
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'PUT',
+            })
+        filename = self.headers.get("File")
+        tmp = form.file
+        result = tmp.read()
+        fl = open(filename, "wb")
+        fl.write(result)
+        fl.close()
+        self.send_response(200)
+
+
+def serve():
+    PORT = 8000
+    Handler = ServerHandler
+    httpd = MyTCPServer(("", PORT), Handler)
+    print "serving at port", PORT
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    serve()
