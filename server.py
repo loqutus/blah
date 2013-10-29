fbbe6e6335445e1f2ced204e5f647b13a7472cb4
Gustomyasov Ruslan <rusik@4ege.ru>
2013-10-29 17:56:54 +0400

    test mongodb

--- server.py
+++ server.py
@@ -20,27 +20,35 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             environ={'REQUEST_METHOD': 'POST',
                      'CONTENT_TYPE': self.headers['Content-Type'],
             })
-        connection = pymongo.Connection(config.db_host, config.db_port)
-        db = connection.blah
-        collection = db.files
-        md5 = self.headers.get("md5")
-        a = collection.find_one({"md5": md5})
-        if a:
-            print a
-        else:
-            print "not a!"
-            for item in form.list:
-                name = item.name
-                f = open(item.name, "w")
-                f.write(item.value)
-                f.close()
-                md5Local = hashlib.md5(open(item.name).read()).hexdigest()
-            if md5 == md5Local:
-                self.send_response(200)
-            else:
-                self.send_response(500)
-                files = {"name": name, "md5": md5}
-                collection.insert(files)
+
+        #md5 = self.headers.get("md5")
+        #a = collection.find_one({"md5": md5})
+        #if a:
+        #    print a
+        #else:
+        #    print "not a!"
+        for item in form.list:
+            connection = pymongo.Connection(config.db_host, config.db_port)
+            db = connection.blah
+            collection = db.files
+            name = item.name
+            md5 = self.headers.get("md5")
+            a = collection.find_one({"name": name})
+            if a:
+                if a["md5"] == md5:
+                    print "exists!"
+                else:
+                    f = open(item.name, "w")
+                    f.write(item.value)
+                    f.close()
+                    md5Local = hashlib.md5(open(item.name).read()).hexdigest()
+
+                if md5 == md5Local:
+                    files = {"name": name, "md5": md5}
+                    collection.insert(files)
+                    self.send_response(200)
+                else:
+                    self.send_response(500)
 
 
     def do_PUT(self):
@@ -68,13 +76,13 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             self.send_response(500)
 
 
-def serve():
-    os.chdir(config.directory)
-    Handler = ServerHandler
-    httpd = MyTCPServer((config.ip, config.port), Handler)
-    print "serving at port", config.port
-    httpd.serve_forever()
+    def serve():
+        os.chdir(config.directory)
+        Handler = ServerHandler
+        httpd = MyTCPServer((config.ip, config.port), Handler)
+        print "serving at port", config.port
+        httpd.serve_forever()
 
 
-if __name__ == '__main__':
-    serve()
+    if __name__ == '__main__':
+        serve()
