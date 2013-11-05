9873b980279482ac1596048323ceecf15afdccd8
Gustomyasov Ruslan <rusik@4ege.ru>
2013-11-05 16:52:49 +0400

    debugging

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import os
 import hashlib
 import pymongo
 import requests
-import pdb
+#import pdb
 
 
 class MyTCPServer(SocketServer.TCPServer):
@@ -29,55 +29,71 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             name = item.name
             md5 = self.headers.get("md5")
             a = collection.find_one({"name": name})
+            print a
             if a:  #if file exists in mongodb
                 if a["md5"] == md5:  #if md5 in mongodb equals md5 of a file
+                    print "1"
                     if os.path.exists(name):
+                        print "2"
                         md5local = hashlib.md5(open(name).read()).hexdigest()
                         if md5local == md5:
+                            print "3"
                             pass
                         else:
+                            print "4"
                             os.remove(name)
                             f = open(name, "w")
                             f.write(item.value)
                             f.close()
                     else:
+                        print "5"
                         f = open(name, "w")
                         f.write(item.value)
                         f.close()
                     self.send_response(200)
                 else:
+                    print "6"
                     os.remove(name)
                     f = open(name, "w")
                     f.write(item.value)
                     f.close()
                     md5local = hashlib.md5(open(name).read()).hexdigest()
                     if md5 == md5local:
+                        print "7"
                         files = {"name": name, "md5": md5}
                         collection.insert(files)
-                        pdb.set_trace()
+                        #pdb.set_trace()
                         for host in config.hosts:
+                            print host
+                            print "http://" + host + ":" + str(config.port)
+                            print config.directory + "/" + name
                             headers = {'md5': md5}
-                            r = requests.post(host + ":" + str(config.port), files={file: open(name, 'rb')},
-                                              headers=headers)
+                            print headers
+                            r = requests.post("http://" + host + ":" + str(config.port), files={file: open(config.directory + "/" + name, 'rb')}, headers=headers)
                         self.send_response(200)
                     else:
+                        print 8
                         os.remove(name)
                         self.send_response(500)
-            else:  #if file does not exists
+            else:  #if file does not exist
+                print 9
                 f = open(name, "w")
                 f.write(item.value)
                 f.close()
                 md5local = hashlib.md5(open(name).read()).hexdigest()
                 if md5 == md5local:
+                    print 10
                     files = {"name": name, "md5": md5}
                     collection.insert(files)
                     for host in config.hosts:
-                        pdb.set_trace()
+                        print host
+                        print "http://" + host + ":" + str(config.port)
+                        #pdb.set_trace()
                         headers = {'md5': md5}
-                        r = requests.post(host + ":" + str(config.port), files={file: open(name, 'rb')},
-                                          headers=headers)
+                        r = requests.post("http://" + host + ":" + str(config.port), files={file: open(name, 'rb')}, headers=headers)
                     self.send_response(200)
                 else:
+                    print "11"
                     os.remove(name)
                     self.send_response(500)
 
