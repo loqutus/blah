6d9a453bf73345fa31e2fcd6a7206fa0afe4f9bc
Gustomyasov Ruslan <rusik@4ege.ru>
2013-12-22 18:12:58 +0400

    quick fix

--- client.py
+++ client.py
@@ -3,16 +3,18 @@ import hashlib
 
 import requests
 
-
-if sys.argv[1] == "upload":
-    for file in sys.argv[1:-1]:
-        #todo:use basename instead of just sys.argv[]
-        md5 = hashlib.md5(open(file).read()).hexdigest()
-        headers = {'md5': md5}
-        print md5
-        print headers
-        print file
-        print sys.argv[-1]
-        r = requests.post(sys.argv[-1], files={file: open(file, 'rb')}, headers=headers)
-        print file
+print "1"
+print sys.argv[0]
+print sys.argv[1]
+file = sys.argv[1]
+print "2"
+#todo:use basename instead of just sys.argv[]
+md5 = hashlib.md5(open(file).read()).hexdigest()
+headers = {'md5': md5}
+print md5
+print headers
+print file
+print sys.argv[-1]
+r = requests.post(sys.argv[-1], files={file: open(file, 'rb')}, headers=headers)
+print file
 print "done"
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-ip = ""
+ip = "127.0.0.1"
 port = 8000
-db_host = "10.8.0.54"
+db_host = "127.0.0.1"
 db_port = 27017
 directory = "/var/lib/blah/data"
 hosts = ['10.0.3.35', '10.0.3.84']
